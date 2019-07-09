f5273255a8a4ae6d7cf6859047155da4939c31e4
Tate <tateshep@gmail.com>
2019-07-09 16:49:09 -0700

    adding postgres

--- hannahchebetphoto/hannahchebetphoto/settings.py
+++ hannahchebetphoto/hannahchebetphoto/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'hannahchebetphoto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "os.getenv('DB_NAME','')",
+        'USER': "os.getenv('DB_USER','')",
+        'PASSWORD': "os.getenv('DB_PASS','')",
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
