84600c78d8be52b3acb1a6dd6aa7ea87e69b0aa0
Tate <tateshep@gmail.com>
2019-07-10 12:43:13 -0700

    working on adding postgres

--- hannahchebetphoto/hannahchebetphoto/settings.py
+++ hannahchebetphoto/hannahchebetphoto/settings.py
@@ -96,9 +96,9 @@ WSGI_APPLICATION = 'hannahchebetphoto.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "os.getenv('DB_NAME','')",
-        'USER': "os.getenv('DB_USER','')",
-        'PASSWORD': "os.getenv('DB_PASS','')",
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASS', ''),
         'HOST': 'localhost',
         'PORT': '5432',
     }
