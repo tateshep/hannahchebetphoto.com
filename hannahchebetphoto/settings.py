45a4c7071e4b476c9dd2db5d7b58afd2335b666a
Tate <tateshep@gmail.com>
2019-08-05 15:43:29 -0700

    adding aws

--- hannahchebetphoto/settings.py
+++ hannahchebetphoto/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'django_comments',
     'django.contrib.sites',
+    'storages',
 
 ]
 
@@ -157,7 +158,7 @@ SITE_ID = 1
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/data/'
@@ -168,3 +169,11 @@ LOGIN_REDIRECT_URL = 'home:index'
 LOGOUT_REDIRECT_URL = 'home:index'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
