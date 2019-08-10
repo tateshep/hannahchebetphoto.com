56e4ffa3d197559c9c05bbe3ea36d80ab3062105
Tate <tateshep@gmail.com>
2019-08-10 14:10:06 -0700

    S3 stuff

--- hannahchebetphoto/settings.py
+++ hannahchebetphoto/settings.py
@@ -200,6 +200,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
+AWS_S3_REGION_NAME = 'us-west-2'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
