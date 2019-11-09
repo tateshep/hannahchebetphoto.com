53cf44e1ada531199778a52772c37bbb864beee8
Tate <tateshep@gmail.com>
2019-11-09 11:37:16 -0800

    added virtualenv

---
+++ ENVhannahchebetphoto/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tateshepherd/Documents/web-design-projects/Hannah-website/django-hannah/hannahchebetphoto/ENVhannahchebetphoto/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
